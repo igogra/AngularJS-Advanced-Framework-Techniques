53ca4c3f4dad0c0008073fecf37b84e27ccb431f
Iván Gómez <ivangomez@mmedios.local>
2017-01-05 09:50:35 +0100

    Add Module 3 Self-Assessment Lab

---
+++ Mod3Lab/app/labController.js
@@ -0,0 +1,30 @@
+angular.module('app').controller('labController', [function() {
+    var vm = this;
+
+    vm.alerts = [];
+    vm.selectedDate = new Date();
+    vm.dateOptions = {
+        showWeeks: false,
+        minDate: new Date()
+    };
+
+    vm.addDanger = addDanger;
+    vm.addWarning = addWarning;
+    vm.dismissAlert = dismissAlert;
+
+    function addDanger() {
+        addAlert('danger', 'Danger, Will Robinson! Danger!');
+    }
+
+    function addWarning() {
+        addAlert('warning', 'Warning! Warning! Alien approaching!');
+    }
+
+    function addAlert(type, text) {
+        vm.alerts.push({ type: type, text: text });
+    }
+
+    function dismissAlert(index) {
+        vm.alerts.splice(index, 1);
+    }
+}]);
---
+++ Mod3Lab/readme.md
@@ -0,0 +1,13 @@
+#User Story
+
+As a user, I want the ability to select a date with a date picker that is either today's date or in the future and display the selected date so that I can see the selected date.
+
+##Acceptance Criteria
+
+- A Datepicker control is displayed on the page
+- The Datepicker control is initially bound to today's date
+- Don't allow dates in the past
+- Hide the week numbers
+- Display the selected date in a <pre> tag and apply the date filter
+
+[See my solution web page](https://igogra.github.io/AngularJS-Advanced-Framework-Techniques/Mod3Lab/)
